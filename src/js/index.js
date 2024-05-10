1365d4ebfe065f698a6cb4dc12f833734ba1f1cc
Nahuel Aranda <facundoaranda@gmail.com>
2024-05-09 21:08:18 -0300

    formulario requerimiento de preentrega 9-5-2024

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,12 @@ import FooterComponent from './components/footer-component.js';
 
 customElements.define('navbar-component', NavbarComponent);
 customElements.define('footer-component', FooterComponent);
+
+
+const formulario = document.querySelector("form")
+    
+    formulario.addEventListener("submit", function (evento) {
+        evento.preventDefault()
+    const data = Object.fromEntries(new FormData(formulario))
+    console.log(data); 
+    } )
